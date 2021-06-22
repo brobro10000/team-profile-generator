6fb5cf6bf45a26312565c2b6e677e40b038ff118
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-22 16:58:43 -0400

    Completed output HTML for expected output
    Included a carousel for each manager, engineer and intern. Requires simply catching and passing the data to the JS to render the HTML

--- dist/script.js
+++ dist/script.js
@@ -1,26 +1,53 @@
+const MAX_CARDS_INDEX = 2
 //SAME JS START HEADER
 function header() {
     $("<header>").attr({ "id": "headerContainer" }).appendTo(document.body)
     $("<h3>").text("My Team").attr({ "class": "text-center" }).appendTo("#headerContainer")
 }
 //END HEADER
-function main() {
+ function main() {
     $("<main>").attr({"id":"main"}).appendTo(document.body)
-    $("<section>").attr({"id":"employeeCards"}).appendTo("#main")
-    $("<div>").attr({ "id": "cardDeck", "class": "card-deck" }).appendTo("#employeeCards")
+
 }
-function cards(i) {
-    $("<div>").attr({ "id": "cardContainer" + i, "class": "card ml-1 mr-1 text-center" }).appendTo("#cardDeck")
-         $("<div>").attr({ "id": "cardContainerHeader" + i, "class": "card cardHeader ml-1 mr-1 text-center" }).appendTo("#cardContainer"+i)
-        $("<h5>").text("").attr({ "id": "name" + i, "class": "cardText" }).appendTo("#cardContainerHeader" + i)
-        $("<img>").attr({ "id": "icon" + i, "src": ""}).appendTo("#name" + i)
-        $("<h5>").text("role"+i).attr({ "id": "role" + i, "class": "cardText" }).appendTo("#cardContainerHeader" + i)
-        $("<div>").attr({ "id": "cardContainerContent" + i, "class": "card ml-1 mr-1 text-center" }).appendTo("#cardContainer"+i)
-        $("<h6>").text("ID"+i).attr({ "id": "ID" + i, "class": "cardText" }).appendTo("#cardContainerContent" + i)
-        $("<h6>").text("Email"+i).attr({ "id": "Email" + i, "class": "cardText" }).appendTo("#cardContainerContent" + i)
-        $("<h6>").text("GorS"+i).attr({ "id": "GithubSchool" + i, "class": "cardText" }).appendTo("#cardContainerContent" + i)
+function cards(i,role) {
+    $("<div>").attr({ "id": role+"cardContainer" + i, "class": "card ml-1 mr-1 text-center" }).appendTo("#"+role+"carouselPosition"+i)
+         $("<div>").attr({ "id": role+"cardContainerHeader" + i, "class": "card cardHeader ml-1 mr-1 text-center" }).appendTo("#"+role+"cardContainer"+i)
+        $("<h5>").text("").attr({ "id": role+"name" + i, "class": "cardText" }).appendTo("#"+role+"cardContainerHeader" + i)
+        $("<img>").attr({ "id": role+"icon" + i, "src": ""}).appendTo("#"+role+"name" + i)
+        $("<h5>").text("role"+i).attr({ "id": role+"role" + i, "class": "cardText" }).appendTo("#"+role+"cardContainerHeader" + i)
+        $("<div>").attr({ "id": role+"cardContainerContent" + i, "class": "card ml-1 mr-1 text-center" }).appendTo("#"+role+"cardContainer"+i)
+        $("<h6>").text("ID"+i).attr({ "id": role+"ID" + i, "class": "cardText" }).appendTo("#"+role+"cardContainerContent" + i)
+        $("<h6>").text("Email"+i).attr({ "id": role+"Email" + i, "class": "cardText" }).appendTo("#"+role+"cardContainerContent" + i)
+        $("<h6>").text("GorS"+i).attr({ "id": role+"GithubSchool" + i, "class": "cardText" }).appendTo("#"+role+"cardContainerContent" + i)
 }
+
 header()
 main()
-for(var i = 0;i < 5; i++)
-cards(i)
+carouselCreator(1,"manager")
+carouselCreator(4,"engineer")
+carouselCreator(10,"intern")
+
+function carouselCreator(innerItems,role){
+    $("<div>").attr({"id":role+"CarouselContainer", "class":"carousel slide carouselContainer"}).appendTo("#main")
+    $("<div>").attr({"id":role+"CarouselInner", "class":"carousel-inner"}).appendTo("#"+role+"CarouselContainer")
+    for(var i = 0;i<innerItems;i++){
+        carouselInner(i,role)
+        cards(i,role)
+    }
+    carouselButtons(role)
+}
+function carouselButtons(role) {
+    $("<a>").attr({"id":role+"prevButton","class":"carousel-control-prev","href":"#"+role+"CarouselContainer", "role":"button", "data-slide":"prev"}).appendTo("#"+role+"CarouselContainer")
+    $("<span>").attr({"class":"carousel-control-prev-icon", "aria-hidden":"true"}).appendTo("#"+role+"prevButton")
+    $("<span>").attr({"class":"sr-only"}).text("Prev").appendTo("#"+role+"prevButton")
+    $("<a>").attr({"id":role+"nextButton","class":"carousel-control-next","href":"#"+role+"CarouselContainer", "role":"button", "data-slide":"next"}).appendTo("#"+role+"CarouselContainer")
+    $("<span>").attr({"class":"carousel-control-next-icon", "aria-hidden":"true"}).appendTo("#"+role+"nextButton")
+    $("<span>").attr({"class":"sr-only"}).text("Next").appendTo("#"+role+"nextButton")
+}
+function carouselInner(i,role){
+    if(i == 0)
+    $("<div>").attr({"id":role+"carouselPosition"+i,"class":"carousel-item active"}).appendTo("#"+role+"CarouselInner")
+    else {
+    $("<div>").attr({"id":role+"carouselPosition"+i,"class":"carousel-item"}).appendTo("#"+role+"CarouselInner")
+    }
+}
